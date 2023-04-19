71d2d7eeaaa72fb32231e8e5c8bd7f224e043540
afm22r <alejandro.fuentes@metaenlace.com>
2023-04-19 14:25:40 +0200

    feat: Añadida api para buscar por 1 id, y añadia api para guardar libros

--- src/main/java/com/example/obrestdatajpa/controllers/BookController.java
+++ src/main/java/com/example/obrestdatajpa/controllers/BookController.java
@@ -2,10 +2,12 @@ package com.example.obrestdatajpa.controllers;
 
 import com.example.obrestdatajpa.entities.Book;
 import com.example.obrestdatajpa.repositories.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -23,14 +25,32 @@ public class BookController {
 
     /**
      * http://localhost:8081/api/books
+     *
      * @return
      */
     @GetMapping("/api/books")
-    public List<Book> findAll(){
+    public List<Book> findAll() {
         return bookRepository.findAll();
     }
 
-//    public  Book findOneById(Long id){
-//
-//    }
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity<Book> findOneById(@PathVariable Long id) {
+        Optional<Book> bookopt = bookRepository.findById(id);
+        // opcion 1 devuelve book si existe, en caso contrario devuelve nullo
+        if (bookopt.isPresent())
+            return ResponseEntity.ok(bookopt.get());
+        else
+            return ResponseEntity.notFound().build();
+        //  return bookopt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    //guardar el libro recibido por parametro en la base de datos
+    @PostMapping("/api/books")
+    public Book create(@RequestBody Book book, @RequestHeader HttpHeaders headers){
+        System.out.println(headers.get("User-Agent"));
+        return bookRepository.save(book);
+    }
+
+
 }
